196a4e096620927bf0a95589d33dcb0ca4666b00
Ryan McCarlie <ryan.mccarlie@aerobotics.com>
2022-04-08 01:25:33 +0200

    feat: Add DatabaseConnection class

--- ticker_data_streaming.py
+++ ticker_data_streaming.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import psycopg2
 from dotenv import load_dotenv
 
-from constants import TABLE_NAME, BID_ASK_TABLE_FIELDS
+from constants import TABLE_NAME, SYMBOL_SPREAD_TABLE_FIELDS
 from websocket_ftx.client import FtxWebsocketClient
 
 load_dotenv()
@@ -24,29 +24,31 @@ def unix_timestamp_to_datetime(unix_timestamp):
     if unix_timestamp is None:
         return None
     date_time = datetime.utcfromtimestamp(unix_timestamp)
-    date_time_str = date_time.strftime("%Y/%m/%d %H:%M:%S")
+    date_time_str = date_time.strftime("%Y/%m/%d %H:%M:%S.%f")
     return date_time_str
 
 
-def write_entry_data_to_db(cursor, symbol, bid_ask_data):
+def write_entry_data_to_db(db_connection, symbol, bid_ask_data):
     datetime_str = unix_timestamp_to_datetime(bid_ask_data.get("time"))
 
     values_str_list = ",".join([str(field) for field in bid_ask_data.values()])
     bid_ask_entry_values = f"{values_str_list},'{symbol}','{datetime_str}'"
 
-    sql = f"INSERT INTO {TABLE_NAME} ({','.join(BID_ASK_TABLE_FIELDS)}) " \
+    sql = f"INSERT INTO {TABLE_NAME} ({','.join(SYMBOL_SPREAD_TABLE_FIELDS)}) " \
           f"VALUES ({bid_ask_entry_values})"
     main_logger.info(f"Executing SQL: {sql}")
-    cursor.execute(sql)
-    db_cursor.commit()
+    print(f"Executing SQL: {sql}")
+    db_connection.cursor.execute(sql)
+    db_connection.connection.commit()
 
 
-async def subscribe_to_symbol_ws_and_write_to_db(db_cursor, websocket, symbol, symbol_id, ticker_interval):
+async def subscribe_to_symbol_ws_and_write_to_db(db_connection, websocket, symbol, symbol_id,
+                                                 ticker_interval):
     while True:
         bid_ask_data = websocket.get_ticker(market=symbol)
         if len(bid_ask_data) > 0:
             # Write to DB
-            write_entry_data_to_db(db_cursor, symbol, bid_ask_data)
+            write_entry_data_to_db(db_connection, symbol, bid_ask_data)
         else:
             main_logger.info(f"No data available for {symbol}")
 
@@ -54,11 +56,11 @@ async def subscribe_to_symbol_ws_and_write_to_db(db_cursor, websocket, symbol, s
         await asyncio.sleep(ticker_interval)
 
 
-async def stream_and_write_data_to_db(cursor, websocket, symbols, ticker_interval):
+async def stream_and_write_data_to_db(db_connection, websocket, symbols, ticker_interval):
     async_symbol_streaming_coroutines = []
     for symbol_id, symbol in enumerate(symbols):
         streaming_coroutine = subscribe_to_symbol_ws_and_write_to_db(
-            db_cursor=cursor, websocket=websocket, symbol=symbol,
+            db_connection=db_connection, websocket=websocket, symbol=symbol,
             symbol_id=symbol_id, ticker_interval=ticker_interval
         )
         async_symbol_streaming_coroutines.append(streaming_coroutine)
@@ -83,7 +85,7 @@ def init_ftx_websocket_client():
     return ws
 
 
-def write_symbol_data_to_postgres_db(cursor, symbols):
+def write_symbol_data_to_postgres_db(db_connection, symbols):
     """
     Given a bunch of symbols - create websockets for each symbol and stream data into a database
     """
@@ -94,7 +96,7 @@ def write_symbol_data_to_postgres_db(cursor, symbols):
     ticker_interval = 1
 
     try:
-        asyncio.run(stream_and_write_data_to_db(cursor, ftx_websocket, symbols, ticker_interval))
+        asyncio.run(stream_and_write_data_to_db(db_connection, ftx_websocket, symbols, ticker_interval))
 
     except KeyboardInterrupt:
         logging.info(f"Stopped - Keyboard interrupt")
@@ -109,35 +111,42 @@ def get_all_table_data(cursor, table_name):
     """
     sql = f"SELECT * from {table_name};"
     cursor.execute(sql)
-    db_results = db_cursor.fetchall()
+    db_results = cursor.fetchall()
     return db_results
 
 
-def get_postgres_connection_and_cursor():
+class DatabaseConnection:
     """
-    Establishing the connection
+    Create the database connection to postgres db
     """
-    # TODO: Fetch from ENV vars from .env or docker env
-    connection = psycopg2.connect(
-        database="postgres", user='postgres', password='postgres', host='localhost', port='5432'
-    )
-    # Creating a cursor object using the cursor() method
-    cursor = connection.cursor()
 
-    return connection, cursor
+    def __init__(self, database="postgres", user='postgres', password='postgres', host="localhost",
+                 port="5432", enable_autocommit=False):
+        self.connection = psycopg2.connect(
+            database=database, user=user, password=password, host=host, port=port
+        )
+        self.cursor = self.connection.cursor()
+
+        if enable_autocommit:
+            self.enable_autocommit()
+
+    def enable_autocommit(self):
+        self.connection.autocommit = True
 
 
 if __name__ == "__main__":
     # TODO: Fetch symbols from a config file ?
     symbols_list = ["ETH/USD", "SOL/USD"]
-    main_logger.info(f"Pulling stream data for : {symbols_list}")
+    main_logger.info(f"Streaming data for : {symbols_list}")
 
-    postgres_conn, db_cursor = get_postgres_connection_and_cursor()
+    # postgres_conn, db_cursor = get_postgres_connection_and_cursor()
 
-    get_postgres_current_data = False
+    db_connection = DatabaseConnection(enable_autocommit=False)
+
+    get_postgres_current_data = True
     if get_postgres_current_data:
-        results = get_all_table_data(db_cursor, TABLE_NAME)
+        results = get_all_table_data(db_connection.cursor, TABLE_NAME)
         for row in results:
             main_logger.info(f"{row}")
 
-    write_symbol_data_to_postgres_db(db_cursor, symbols_list)
+    write_symbol_data_to_postgres_db(db_connection, symbols_list)
